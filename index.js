2bdc1ab88e085edbd85a275d655c50333b010dbb
Davi <davirodrigues8@gmail.com>
2019-02-21 16:00:36 -0300

    Finalizando o desafio

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const app = express()
+// Configurando nunjacks
 nunjucks.configure('views', {
   autoescape: true,
   express: app,
@@ -13,8 +14,33 @@ app.use(express.urlencoded({ extended: false }))
 // Setando template nunjacks
 app.set('view engine', 'njk')
 
+const validatorMiddlware = (req, res, next) => {
+  if (!req.query.idade) {
+    return res.redirect('/')
+  }
+  return next()
+}
+// Rota inicial com formulario de idade
 app.get('/', (req, res) => {
-  return 'Hello world'
+  return res.render('age')
+})
+
+// Rota para checar a idade digitada
+app.post('/check', (req, res) => {
+  if (req.body.age >= 18) {
+    return res.redirect(`/major?idade=${req.body.age}`)
+  }
+  return res.redirect(`/minor?idade=${req.body.age}`)
+})
+
+// Rota para mostrar pagina com mensagem
+app.get('/major', validatorMiddlware, (req, res) => {
+  return res.render('major', { idade: req.query.idade })
+})
+
+// Rota para mostrar pagina com mensagem
+app.get('/minor', validatorMiddlware, (req, res) => {
+  return res.render('minor', { idade: req.query.idade })
 })
 
 app.listen(3000)
